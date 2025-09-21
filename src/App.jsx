878eeaf1fb28b5f7a9ac024fe6b4cd8319b20485
sebastianmartinmoreno-hub <sebastianmartinmoreno@gmail.com>
2025-09-21 17:50:09 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className='text-center text-2xl text-white bg-gradient-to-br from-slate-950 via-slate-900 to-slate-800 min-h-screen flex items-center justify-center'>
+      Demon Slayer Tennis Breathing Trainer is working!
+    </div>
+  )
+}
